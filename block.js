28fe094a830a4a4a855a970a77c6edb5b2ed41f8
MA <56746772+mohdlatif@users.noreply.github.com>
2022-06-16 05:12:13 +0800

    Update block.js

--- block.js
+++ block.js
@@ -1,3 +1,7 @@
 /// block.js
+document.addEventListener('DOMContentLoaded', function(event) {
 let video = document.querySelector('[x-for*="video"]'); video.parentNode.parentElement.remove();
 console.log("Hello I'm running via scriptlet injection.");
+});
+
+
