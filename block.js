f5db5fbed5238a7c0f0f150512f72feb1b48960a
MA <56746772+mohdlatif@users.noreply.github.com>
2022-06-18 02:10:41 +0800

    Update block.js

--- block.js
+++ block.js
@@ -3,7 +3,8 @@ console.log("Filter UBO.");
 
 document.addEventListener('DOMContentLoaded', function(event) {
   
-  let video = document.querySelector('[x-for*="video"]'); 
-  video.parentNode.parentElement.remove();
+var x = document.querySelector('[x-html*="video.time"]');
+
+x.parentElement.parentElement.parentElement.parentElement.parentElement.remove();
 
 });
